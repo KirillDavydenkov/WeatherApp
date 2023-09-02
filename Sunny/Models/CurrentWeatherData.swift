cb88a361931ea290aa95c8df7c471c9cd5c136f4
Kirill <130473196+KirillDavydenkov@users.noreply.github.com>
2023-09-02 21:36:16 +0300

    First commit
    Weather app as part of the course from Swiftbook " Работа с API"

---
+++ Sunny/Models/CurrentWeatherData.swift
@@ -0,0 +1,29 @@
+//
+//  CurrentWeatherData.swift
+//  Sunny
+//
+//  Created by Вероника Черных on 16.5.23..
+//  Copyright © 2023 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Вероника Черных on 15.5.23..
+//  Copyright © 2023 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "e9414f839df6720f26f4ffb3fb6c635a"
